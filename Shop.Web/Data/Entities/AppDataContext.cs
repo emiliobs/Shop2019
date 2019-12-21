5f0d79cc66755ed6f62b00a4abae83b64884a9d4
emilio barrera <barrera_emilio@hotmail.com>
2019-12-21 23:58:25 +0000

    Add products controles and migration  db

---
+++ Shop.Web/Data/Entities/AppDataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Data.Entities
+{
+    public class AppDataContext:DbContext
+    {
+        public AppDataContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
