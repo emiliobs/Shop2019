ce339549c3b3f9459d16c475f2ede912fb8ec587
emilio barrera <barrera_emilio@hotmail.com>
2019-12-29 20:41:01 +0000

    Bindig properties of LoginView with LoginViewModel

--- Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
+++ Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -1,10 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
 
 namespace Shop.UIForms.ViewModels
 {
     public class LoginViewModel
     {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICommand LoginCommand => new RelayCommand(Login);
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Please, You must enter an Email.", "Accept");
+
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Password))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Plese, You must enter a Password.", "Accept");
+
+                return;
+            }
+
+            await App.Current.MainPage.DisplayAlert("Ok", "Fuck yeah!!!!", "Accept");
+
+
+        }
     }
 }
